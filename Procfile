5e6098c8600ba336e957b8cfa3668c7786e9b66a
AllSafeCybercurity <skull32273227@gmail.com>
2021-01-13 06:16:02 -0800

    second commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn gov_usa.wsgi
--- gov_usa/settings.py
+++ gov_usa/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -49,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'gov_usa.urls'
@@ -126,6 +130,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # myaccount.google.com/lesssecureapps
 # accounts.google.com/DisplayUnlockCaptcha
@@ -137,3 +142,5 @@ STATICFILES_DIRS = [
 #EMAIL_HOST_PASSWORD = ''
 #EMAIL_USE_TLS = True
 # EMAIL_USE_SSL = False 
+
+django_heroku.settings(locals())
